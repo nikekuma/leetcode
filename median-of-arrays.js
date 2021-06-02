48f566a253d09b463dd429ccf8cddaf32ef9eb4b
niket-kumar <niket.kumar@efi.com>
2021-06-02 15:22:36 +0530

    median arrays in logn

--- median-of-arrays.js
+++ median-of-arrays.js
@@ -66,35 +66,64 @@ var findMedianSortedArrays = function(num1, num2) {
 };
 
 var findMedianSortedArrays = function(num1, num2) {
-	let m = num1.length;
-  let n = num2.length;
-  let i = 0;
-  let j = 0;
-  let count = 0;
-  let m1 = -1;
-  let m2 = -1;
-  if ((m + n) % 2 === 0) {
-  	for (let count = 0; count <= (m + n)/2; count++) {
-    	m2 = m1;
-      if (i !== m && j !== n) {
-      	m1 = num1[i] < num2[j] ? num1[i++] : num2[j++];
-      } else if (i < m) {
-      	m1 = num1[i++];
-      } else {
-      	m1 = num2[j++];
-      }
+    let m = num1.length;
+    let n = num2.length;
+    let i = 0;
+    let j = 0;
+    let m1 = -1;
+    let m2 = -1;
+    if ((m + n) % 2 === 0) {
+        for (let count = 0; count <= (m + n)/2; count++) {
+            m2 = m1;
+        if (i !== m && j !== n) {
+            m1 = num1[i] < num2[j] ? num1[i++] : num2[j++];
+        } else if (i < m) {
+            m1 = num1[i++];
+        } else {
+            m1 = num2[j++];
+        }
+        }
+        return (m1 + m2)/2;
+    } else {
+        for (let count = 0; count <= (m + n)/2; count++) {
+            if (i !== m && j !== n) {
+            m1 = num1[i] < num2[j] ? num1[i++] : num2[j++];
+        } else if (i < m) { 
+            m1 = num1[i++];
+        } else {
+            m1 = num2[j++];
+        }
+        }
+        return m1;
     }
-    return (m1 + m2)/2;
-  } else {
-  	for (let count = 0; count <= (m + n)/2; count++) {
-    	if (i !== m && j !== n) {
-      	m1 = num1[i] < num2[j] ? num1[i++] : num2[j++];
-      } else if (i < m) { 
-      	m1 = num1[i++];
+};
+
+
+var findMedianSortedArrays = function(num1, num2) {
+	if (num1.length > num2.length) {
+  	[num1, num2] = [num2, num1];
+  }
+  let l1 = 0;
+  let h1 = num1.length;
+  let totalLen = num1.length + num2.length;
+  while (l1 <= h1) {
+  	i1 = Math.ceil((l1 + h1)/2);
+    i2 = Math.ceil(totalLen/2 -i1); 
+    minright1 = i1 === num1.length ? Number.MAX_SAFE_INTEGER : num1[i1]
+    maxleft1 = i1 === 0 ? Number.MIN_SAFE_INTEGER : num1[i1 - 1];
+    minright2 = i2 === num2.length ? Number.MAX_SAFE_INTEGER : num2[i2]
+    maxleft2 = i2 === 0 ? Number.MIN_SAFE_INTEGER : num2[i2 - 1];
+    
+    if (maxleft1 <= minright2 && minright1 >= maxleft2) {
+    	if ((totalLen % 2) === 0) {
+       return (Math.max(maxleft1, maxleft2) + Math.min(minright1, minright2))/2;
       } else {
-      	m1 = num2[j++];
+      	return Math.max(maxleft1, maxleft2);
       }
+    } else if (maxleft1 > minright2) {
+    	h1 = i1 - 1;
+    } else {
+    	l1 = i1 + 1;
     }
-    return m1;
   }
 };
